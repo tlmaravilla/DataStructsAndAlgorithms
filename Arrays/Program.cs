dd931578b6efd32b8f057f8abba3a36d4ac8245a
Tom Maravilla <tlmaravilla@outlook.com>
2020-02-03 21:45:13 -0700

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Exercise 1
+
+            var supArray = new SuperArray(5);
+            supArray.Insert(10);
+            supArray.Insert(20);
+            supArray.Insert(30);
+            supArray.Insert(40);
+            supArray.Insert(50);
+            supArray.Insert(60);
+            supArray.Insert(70);
+            supArray.Insert(80);
+
+            supArray.Print();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Arrays/SuperArray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public class SuperArray
+    {
+        private int[] items;
+        private int count;
+
+        public SuperArray(int length)
+        {
+            items = new int[length];
+        }
+
+        public void Insert(int item)
+        {
+            // If the array is full, resize
+            IsResizeRequired();
+
+            // Get the current index and increment it, then add item
+            items[count++] = item;
+        }
+
+        public void Print()
+        {
+            foreach (var item in items)
+            {
+                if(item > 0)
+                    Console.WriteLine(item);
+            }
+        }
+
+        private void IsResizeRequired()
+        {
+            if(items.Length == count)
+            {
+                var resize = new int[count * 2];
+
+                for (int i = 0; i < items.Length; i++)
+                {
+                    resize[i] = items[i];
+                }
+                items = resize;
+            }
+        }
+    }
+}
